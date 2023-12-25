891cb12f874e0d13ef107cb8908a1ed46287b791
thipham2018 <trangphamtmp@gmail.com>
2023-12-25 11:13:14 -0500

    connect database

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class LoginForm extends JDialog{
     private JTextField tfEmail;
@@ -50,6 +51,9 @@ public class LoginForm extends JDialog{
         final String PASSWORD = "Danang2018@";
 
 try { Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+    // connected to database successfully..
+
+    Statement stmt = conn.createStatement();
 
 }
 catch (Exception e){
